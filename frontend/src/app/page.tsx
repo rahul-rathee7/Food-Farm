a77980e636349f9e634537a1269ddb38967585e5
ajayjaat01 <ajaybhanwala730@gmail.com>
2025-06-25 10:36:39 +0530

    aj push

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Main from './components/Main';
 import Categories from './components/Categories';
+import  Cards  from './components/Cards';
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Main />
       <div className='flex gap-2 w-11/12 mx-auto'>
         <Categories />
+        <Cards />
       </div>
     </div>
   );
