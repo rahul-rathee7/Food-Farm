3340166629215c64abbc1dce3340f070f46c25f1
Rahul Rathee <ratheerahul602@gmail.com>
2025-06-25 11:17:04 +0530

    ajayfuck

--- frontend/src/app/components/Cards.tsx
+++ frontend/src/app/components/Cards.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Cards = () => {
   return (
     <div>
-      
       <div>Cards Rathee</div>
       <div>Cards Rathee</div>
+      <p>ratheee ddddd</p>
     </div>
   )
 }
