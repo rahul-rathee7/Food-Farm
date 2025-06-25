17f56961a306a8e4b2153090429a641cdf9814c0
ajayjaat01 <ajaybhanwala730@gmail.com>
2025-06-25 13:00:33 +0530

    ajay card 2

--- frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
@@ -2,7 +2,7 @@
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
   "static/chunks/node_modules_759038ed._.js",
-  "static/chunks/_fbac64b3._.js"
+  "static/chunks/_d529eeab._.js"
 ],
     source: "dynamic"
 });
