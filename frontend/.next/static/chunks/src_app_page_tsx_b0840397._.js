f0c1a803b1f1337df7aac8052076acb29fceaba6
ajayjaat01 <ajaybhanwala730@gmail.com>
2025-06-25 15:12:29 +0530

    ajay

--- frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
@@ -1,8 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/node_modules_759038ed._.js",
-  "static/chunks/_d529eeab._.js"
+  "static/chunks/_d529eeab._.js",
+  "static/chunks/node_modules_759038ed._.js"
 ],
     source: "dynamic"
 });
