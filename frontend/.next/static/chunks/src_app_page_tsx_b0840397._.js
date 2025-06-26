16be0ca6335680aa9d216f7f293dff50bda3ee31
Rahul Rathee <ratheerahul602@gmail.com>
2025-06-26 11:24:09 +0530

    AboutInformation section created

--- frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_216d6a9e._.js",
+  "static/chunks/_de69d5aa._.js",
   "static/chunks/node_modules_759038ed._.js"
 ],
     source: "dynamic"
