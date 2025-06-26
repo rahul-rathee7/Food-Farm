9f024d44eb529004defcc904cadef0eea1550ffd
Rahul Rathee <ratheerahul602@gmail.com>
2025-06-26 10:02:45 +0530

    Subscribe section created

--- frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
+++ frontend/.next/static/chunks/src_app_page_tsx_b0840397._.js
@@ -1,7 +1,7 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_ad78d301._.js",
+  "static/chunks/_216d6a9e._.js",
   "static/chunks/node_modules_759038ed._.js"
 ],
     source: "dynamic"
