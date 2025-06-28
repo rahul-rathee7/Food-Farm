6c94ff376fe2487e9f3aea2e1689c1fba9254201
Rahul Rathee <ratheerahul602@gmail.com>
2025-06-28 11:37:05 +0530

    some little changes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,7 @@ let cors = require('cors');
 app.use(cors());
 let PORT = 5000;
 
-app.get('/users', (req, res) => {
-    
-})
+
 
 app.listen(PORT, () => {
     console.log("server is running on " + PORT);
