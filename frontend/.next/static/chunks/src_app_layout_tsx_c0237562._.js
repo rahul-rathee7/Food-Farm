404bf04f6aaf0670ff1ff15e9e83a5d1b8eeff7f
Rahul Rathee <ratheerahul602@gmail.com>
2025-07-03 20:31:53 +0530

    Removed .next from Git tracking

--- frontend/.next/static/chunks/src_app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__8ebb6d4b._.css",
-  "static/chunks/node_modules_next_0766a282._.js",
-  "static/chunks/_d49cb3af._.js"
-],
-    source: "dynamic"
-});
