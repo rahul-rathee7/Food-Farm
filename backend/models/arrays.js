893e27b386a7d8d2d0b9dfd8f97568e7093e9c4a
Rahul Rathee <ratheerahul602@gmail.com>
2025-07-10 10:28:46 +0530

    array.js moved in middleware

---
+++ backend/models/arrays.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();  
+const arr = require('./data.json');
+
+router.get('/', (req, res) => {
+    res.json(arr);                  
+});
+
+module.exports = router;       
